46c6ba65103b833cd072b5e63b784fee32e0c93f
Victor Oliveira <victorfelipe1608@gmail.com>
2019-08-20 22:55:24 -0300

    Correção do PUT e DELETE da model de Usuários

--- app/resources/usuario.py
+++ app/resources/usuario.py
@@ -6,20 +6,18 @@ import re
 
 
 parser = reqparse.RequestParser()
-#Fazer função para simplificar a declaração dos argumentos
-
 parser.add_argument('nome')
 parser.add_argument('nickname')
 parser.add_argument('senha')
 parser.add_argument('email')
 parser.add_argument('papel_id')
+parser.add_argument('verificado')
 
 class Usuarios(Resource):
 
     def get(self, param_usuario):
 
         email = re.match(r"[^@]+@[^@]+\.[^@]+", param_usuario)
-        # print('Tratado: {}'.format(email))
 
         if email is None:
             usuario = Usuario.query.filter_by(id=param_usuario).first()
@@ -43,24 +41,19 @@ class Usuarios(Resource):
 
     def post(self):
         response = parser.parse_args()
-        print('Obtidos: {}'.format(response))
         usuario_cadastrado = Usuario.query.filter_by(nome=response['nome']).first()
-        print('Usuario cad: {}'.format(usuario_cadastrado))
         if usuario_cadastrado != None:
-            print('Usuário {} já cadastrado'.format(response['name']))
-            return 200
+            return jsonify({'status': 200, 'content': 'Usuário cadastrado'})
         usuario = Usuario(nome=response['nome'], nickname=response['nickname'],
         senha=response['senha'], email=response['email'], papel_id=int(response['papel_id']), verificado=False)
-        print('user: {}'.format(usuario))
         if usuario != '':
             db.session.add(usuario)
             db.session.commit()
-            print('Cadastrado {} com sucesso!!!'.format(usuario))
         return 201
 
-    def put(self, usuario_id):
+    def put(self, param_usuario):
         response = parser.parse_args()
-        usuario_selecionado = Usuario.query.filter_by(id=usuario_id).first()
+        usuario_selecionado = Usuario.query.filter_by(id=param_usuario).first()
 
         if response.get('name'):
             usuario_selecionado.name = response['name']
@@ -74,15 +67,20 @@ class Usuarios(Resource):
         if response.get('senha'):
             usuario_selecionado.port = response['senha']
 
+        if response.get('verificado'):
+            if response['verificado'] == 'true':
+                usuario_selecionado.verificado = True
+
+
         if response:
             db.session.commit()
 
-        return jsonify({'Usuário Atualizado':usuario_selecionado.id})
+        return jsonify({'status': 201, 'Usuário Atualizado':usuario_selecionado.id})
 
-    def delete(self, usuario_id):
-        usuario_selecionado = Usuario.query.filter_by(id=usuario_id).first()
+    def delete(self, param_usuario):
+        usuario_selecionado = Usuario.query.filter_by(id=param_usuario).first()
         if usuario_selecionado is None:
-            abort(404, "Usuário {} não está cadastrado".format(usuario_id))
-        db.session.delete(usuario_selecionado)
-        db.session.commit()
-        return jsonify({'Usuário deletado':usuario_selecionado.name})
+            return jsonify({'status': 200, 'content': 'Nenhum registro encontrado'})
+        # db.session.delete(usuario_selecionado)
+        # db.session.commit()
+        return jsonify({'status': 200, 'content':usuario_selecionado.nome})
