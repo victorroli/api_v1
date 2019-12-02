de4baee6817ee2f03ec4a60142496899c23ddbd2
Victor Oliveira <victorfelipe1608@gmail.com>
2019-12-01 21:09:40 -0300

    Alterando url do banco de dados

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SQLALCHEMY_DATABASE_URI = DB_URL
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgres://uarcsovkgfydtq:b9c37dfc96cb6f89f7ed76c5ff48a64a48430764166346852f8a3367ab5b81d0@ec2-174-129-253-42.compute-1.amazonaws.com:5432/d2449tbo2t2dts'#os.environ.get('DATABASE_URL')
     SECRET_KEY = 'super-secret'
 
 class DevelopmentConfig(Config):
