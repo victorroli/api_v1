e3c00dc1b417516d56494f9c176647b3f9fea4f5
Victor Oliveira <victorfelipe1608@gmail.com>
2019-12-02 22:06:27 -0300

    Debug para correção de acesso do index

--- app/resources/labs.py
+++ app/resources/labs.py
@@ -86,9 +86,9 @@ class Labs(Resource):
 
             for equipamento in response['equipamentos']:
                 objeto = ast.literal_eval(equipamento)
-                print('Objeto: ', objeto)
-                print('Equipamento: ', equipamento)
-                if equipamento['id']:
+                # print('Objeto: ', objeto)
+                # print('Equipamento: ', equipamento)
+                if objeto['id']:
                     equipamentoBuscado = Equipamento.query.filter_by(id=objeto['id']).first()
 
                     if equipamentoBuscado.nome != objeto['nome']:
