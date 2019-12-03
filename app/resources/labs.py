9a05719f656cd9a1c28dc58ba28c4928b72ce591
Victor Oliveira <victorfelipe1608@gmail.com>
2019-12-02 21:47:56 -0300

    Ajuste da checagem do id do equipamento

--- app/resources/labs.py
+++ app/resources/labs.py
@@ -87,7 +87,7 @@ class Labs(Resource):
             for equipamento in response['equipamentos']:
                 objeto = ast.literal_eval(equipamento)
                 print('Objeto: ', objeto['nome'])
-                if equipamento['id'] != '':
+                if equipamento['id']:
                     equipamentoBuscado = Equipamento.query.filter_by(id=objeto['id']).first()
 
                     if equipamentoBuscado.nome != objeto['nome']:
